5dad9b4766fd023e57819338579a43e18e31273c
Pratikto Genang Pamukti <pratikto@gmail.com>
2026-07-18 16:17:50 +0700

    Sprint 5 - Hero Section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log("Website Undangan Pernikahan");
+// Mengunci scroll saat cover tampil
+document.body.classList.add("lock");
+
+// Mengambil elemen dari HTML
+const openButton = document.getElementById("openInvitation");
+const cover = document.getElementById("cover");
+const app = document.getElementById("app");
+const navigation = document.querySelector(".bottom-nav");
+
+// Event ketika tombol diklik
+openButton.addEventListener("click", function () {
+
+    // Membuat cover menghilang perlahan
+    cover.style.opacity = "0";
+
+    // Menunggu animasi selesai
+    setTimeout(function () {
+
+        // Menyembunyikan cover
+        cover.style.display = "none";
+
+        // Menampilkan isi website
+        app.style.display = "block";
+
+        // Menampilkan navigasi
+        navigation.style.display = "flex";
+
+        // Mengaktifkan scroll
+        document.body.classList.remove("lock");
+
+    }, 600);
+
+});
